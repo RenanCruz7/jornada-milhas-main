1b609a2c68a91965adf800e7d9e6e95f45619bec
RenanCruz7 <renancruz199@gmail.com>
2024-08-02 20:57:23 -0300

    Realizando consulta a API

--- src/app/core/services/passagens.service.ts
+++ src/app/core/services/passagens.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Resultado } from '../types/type';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PassagensService {
+  api : string = environment.apiUrl;
+  constructor( private httpClient: HttpClient) { }
 
-  constructor() { }
+  getPassagens(search: any): Observable<Resultado>{
+    const params = search
+    return this.httpClient.get<Resultado>(this.api + '/passagem/search',{params});
+  }
+
 }
